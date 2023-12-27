1a185d34c8d4682556d9e3089e37654e018855ec
Killercoda <accounts+github@killercoda.com>
2023-12-27 13:43:43 +0100

    removed k3s playgorund

--- playground-k3s/background.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# wait fo k8s ready
-while ! kubectl get nodes | grep -w "Ready"; do
-  echo "WAIT FOR NODES READY"
-  sleep 1
-done
-touch /ks/.k8sfinished
--- playground-k3s/foreground.sh
+++
@@ -1,1 +0,0 @@
-FILE=/ks/wait-background.sh; while ! test -f ${FILE}; do clear; sleep 0.1; done; bash ${FILE}
